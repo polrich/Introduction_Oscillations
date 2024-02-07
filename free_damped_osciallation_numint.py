22e8e4c9ac399c0666930a479b4f28565ef831de
polrich <135112471+polrich@users.noreply.github.com>
2024-02-07 14:02:36 +0100

    Add files via upload

--- free_damped_osciallation_numint.py
+++ free_damped_osciallation_numint.py
@@ -1,3 +1,5 @@
+import matplotlib.pylab as pylab
+from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib qt
@@ -5,58 +7,44 @@ import matplotlib.pyplot as plt
 # Close all open figures
 plt.close('all')
 
-def damped_oscillation(t, omega_0, D, x_0, v_0):
-    omega_d = omega_0 * np.sqrt(1 - D**2)
-    if D < 1:
-        A = np.sqrt((omega_d**2 * x_0**2 + (v_0**2+D*omega_0*x_0)**2)/omega_d**2)
-        phi = np.arctan(omega_d*x_0/(v_0+D*omega_0*x_0))
-        
-        x = A * np.exp(-D * omega_0 * t) * np.sin(omega_d * t + phi)
-        
-    elif D>1:
-        l_1 = -D*omega_0 - omega_0*np.sqrt(D**2 -1)
-        l_2 = -D*omega_0 + omega_0*np.sqrt(D**2 -1)
-        a_1 = (-v_0 + (-D + np.sqrt(D**2 -1))*omega_0*x_0)/(2*omega_0*np.sqrt(D**2-1))
-        a_2 = (+v_0 + (+D + np.sqrt(D**2 -1))*omega_0*x_0)/(2*omega_0*np.sqrt(D**2-1))
-        
-        x = np.exp(-D*omega_0*t)*( a_1 * np.exp( -np.sqrt(D**2-1) * omega_0 * t ) + a_2 * np.exp( np.sqrt(D**2-1) * omega_0 * t ) )
-        
-    elif D==1:
-        a_1 = x_0
-        a_2 = v_0 + omega_0 * x_0
-        
-        x = (a_1 + a_2*t)*np.exp(-omega_0*D*t)
-    return x
+from numpy import array  # Add this line to import the array function
 
-# Parameters
-A = 1.0          # Amplitude
-omega_0 = 1.0    # Angular frequency
+def MassSpringDamper(state, t, omega_0, D):
+    # unpack the state vector
+    x, xd = state  # displacement,x and velocity x'
+    # compute acceleration xdd = x''
+    xdd = -2 * D * omega_0 * xd - omega_0 * x
+    return [xd, xdd]
+
+k = 1  # spring constant, kN/m
+m = 1  # mass, Kg
+omega_0 = np.sqrt(k / m)  # frequency
 T = 2*np.pi/omega_0
-Ds = [0.05, 1, 2]  # Different values of Damping ratio
-
-# Format the legend labels dynamically
-legs = [f'D={D}' for D in Ds]
-
-# Time values
-t_end = 20
-t = np.linspace(0, t_end, 1000)
-
-# Plotting for different D values
-plt.figure(figsize=(15, 8))
-for D, leg in zip(Ds, legs):
-    x = damped_oscillation(t, omega_0, D, 1, 0)
-    plt.plot(t, x, label=leg)
     
+state0 = [1, 0]  # initial conditions [x0 , v0]  [m, m/sec]
+ti = 0.0  # initial time
+tf = 20.0  # final time
+step = 0.001  # step
+t = np.arange(ti, tf, step)
+
+# Plotting displacement for D = 0.05, 1, 2
+pylab.rcParams['figure.figsize'] = (15, 12)
+pylab.rcParams['font.size'] = 18
+fig, ax1 = pylab.subplots()
+
+for D_value in [0.05, 1, 2]:
+    state = odeint(MassSpringDamper, state0, t, args=(omega_0, D_value,))
+    x = array(state[:, [0]])
+    ax1.plot(t, x , label=f'D={D_value}', linewidth=2.0)
+
 # Set xticks at multiples of the period T with labels
-xticks = np.arange(0, t_end, T)
-plt.xticks(xticks, labels=[f'{i} T' for i in range(len(xticks))])
+xticks = np.arange(0, tf, T)
+ax1.set_xticks(xticks)
+ax1.set_xticklabels([f'{i} T' for i in range(len(xticks))])
 
-
-
-plt.title('Harmonic, Free, Damped Oscillation', fontsize=16)
-plt.xlabel('t', fontsize=16)
-plt.ylabel('x', fontsize=16)
-plt.legend(fontsize=16)
-plt.grid(True)
-# plt.xticks([]) 
-plt.show()
+pylab.title('Harmonic, Free, Damped Oscillation', fontsize=16)
+ax1.set_xlabel('t', fontsize=16)
+ax1.set_ylabel('x', fontsize=16)
+ax1.legend(fontsize=16)
+pylab.grid()
+pylab.show()
